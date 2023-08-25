bc3bbf1b7a181f8a3f4079043e7f0f4909b74d6b
Henry Heino <personalizedrefrigerator@gmail.com>
2023-08-24 17:08:11 -0700

    Update default vimrc

--- src/vimrc.ts
+++ src/vimrc.ts
@@ -12,15 +12,19 @@ const getVimrcPath = async () => {
 const defaultVimrcContent = `
 " A .vimrc-like file. At present, very little is supported.
 " You can use the commands:
-"   inoremap
-"   vnoremap
-"   nnoremap
-"   imap
-"   nmap
-"   vmap
+"   inoremap, vnoremap, nnoremap
+"   imap, nmap, vmap
 "   unmap
-" Because commands are passed directly to CodeMirror's ViM API,
+" Because commands are passed directly to CodeMirror's VIM API,
 " insert-mode mappings seem to be unable to insert text.
+"
+" Examples:
+" Allow copying/pasting with ctrl+c and ctrl+v when in insert mode.
+"   unmap <C-c>
+"   unmap <C-v>
+"
+" Map jk to Escape in insert mode:
+"   imap jk <Esc>
 `;
 
 export const getVimrcContent = async (): Promise<string> => {
